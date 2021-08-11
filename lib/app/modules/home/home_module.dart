616dac13bf6d5f8b7824e8795890f4038358ff33
Iury Vasconcelos <iury0393@gmail.com>
2021-08-11 14:07:50 -0300

    feat: add repository

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,4 +1,4 @@
-import 'package:loja_hasura/app/modules/home/widgets/card_produto/card_produto_store.dart';
+import 'package:loja_hasura/app/modules/home/repositories/home_repository.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import '../home/home_store.dart';
 
@@ -7,7 +7,7 @@ import 'home_page.dart';
 class HomeModule extends Module {
   @override
   final List<Bind> binds = [
-    Bind.lazySingleton((i) => CardProdutoStore()),
+    Bind.lazySingleton((i) => HomeRepository()),
     Bind.lazySingleton((i) => HomeStore()),
   ];
 
