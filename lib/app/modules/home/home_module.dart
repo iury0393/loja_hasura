1dd1b0a0d2b8940169537e89fdef1ed9d6fc0b19
Iury Vasconcelos <iury0393@gmail.com>
2021-08-11 14:16:09 -0300

    feat: add repository in module and store

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -7,8 +7,8 @@ import 'home_page.dart';
 class HomeModule extends Module {
   @override
   final List<Bind> binds = [
+    Bind.lazySingleton((i) => HomeStore(i.get<HomeRepository>())),
     Bind.lazySingleton((i) => HomeRepository()),
-    Bind.lazySingleton((i) => HomeStore()),
   ];
 
   @override
--- lib/app/modules/home/home_store.dart
+++ lib/app/modules/home/home_store.dart
@@ -1,3 +1,4 @@
+import 'package:loja_hasura/app/modules/home/repositories/home_repository.dart';
 import 'package:mobx/mobx.dart';
 
 part 'home_store.g.dart';
@@ -5,6 +6,9 @@ part 'home_store.g.dart';
 class HomeStore = HomeStoreBase with _$HomeStore;
 
 abstract class HomeStoreBase with Store {
+  final HomeRepository _repository;
+  HomeStoreBase(this._repository);
+
   @observable
   int counter = 0;
 
