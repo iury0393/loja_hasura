7a19ccfa948565191f9f8a4660556b6d43623546
Iury Vasconcelos <iury0393@gmail.com>
2021-08-11 13:44:57 -0300

    feat: Create card widget

--- lib/app/modules/home/widgets/card_produto/card_produto_store.dart
+++ lib/app/modules/home/widgets/card_produto/card_produto_store.dart
@@ -3,13 +3,5 @@ import 'package:mobx/mobx.dart';
 part 'card_produto_store.g.dart';
 
 class CardProdutoStore = _CardProdutoStoreBase with _$CardProdutoStore;
-abstract class _CardProdutoStoreBase with Store {
 
-  @observable
-  int value = 0;
-
-  @action
-  void increment() {
-    value++;
-  } 
-}
+abstract class _CardProdutoStoreBase with Store {}
